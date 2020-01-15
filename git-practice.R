4e10972408ca30608bd62c77e92b05d89f972e56
Maya <maya.sollen@gmail.com>
2020-01-15 13:49:30 +0000

    notes re undoing things in git

--- git-practice.R
+++ git-practice.R
@@ -9,3 +9,6 @@ mydata <- read.csv(heelo.csv)
 ## This is amazing
 
 ##I hate biscuits
+
+## git checkout "name of script" = useful if accidentally deleted parts of the script or overwritten a file
+## https://github.blog/2015-06-08-how-to-undo-almost-anything-with-git/
